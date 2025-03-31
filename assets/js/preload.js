b8d04743f85a5049c1f1d98aaf84041be3dd3a70
Alven-dev <mikolajct@gmail.com>
2025-03-31 14:52:35 +0200

    preload script added

---
+++ assets/js/preload.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", function() {
+    gsap.from("#art", {
+        duration: 2,
+        opacity: 0,
+        x: 50,
+        y: 50
+    });
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+    gsap.from("#itone", {
+        delay: 2,
+        duration: 1.5,
+        opacity: 0,
+        x: 200
+    });
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,3 @@
-window.addEventListener("DOMContentLoaded", function() {
-    gsap.from("#art", {
-        duration: 2,
-        opacity: 0,
-        x: 50,
-        y: 50
-    })
-})
-
-window.addEventListener("DOMContentLoaded", function() {
-    gsap.from("#itone", {
-        delay: 2,
-        duration: 1.5,
-        opacity: 0,
-        x: 200
-    })
-})
 
 document.addEventListener("DOMContentLoaded", function () {
     const burger = document.querySelector(".burger");
