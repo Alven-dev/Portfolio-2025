80b6a21b66646e994262a896053446ab72552085
Alven-dev <mikolajct@gmail.com>
2025-03-31 14:04:28 +0200

    GSAP js animations added and few minor corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+window.addEventListener("DOMContentLoaded", function() {
+    gsap.from("#art", {
+        duration: 2,
+        opacity: 0,
+        x: 50,
+        y: 50
+    })
+})
+
+window.addEventListener("DOMContentLoaded", function() {
+    gsap.from("#itone", {
+        delay: 2,
+        duration: 1.5,
+        opacity: 0,
+        x: 200
+    })
+})
+
 document.addEventListener("DOMContentLoaded", function () {
     const burger = document.querySelector(".burger");
     const navbar = document.querySelector(".navbar.mobile");
